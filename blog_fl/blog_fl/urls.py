e25232d4e0176554178b9846a06d82cd2e988d87
fabi365 <fabianlopezagudelo@gmail.com>
2024-08-29 18:22:50 -0500

    inclusión de las urls
    se incluyeron las urls

--- blog_fl/blog_fl/urls.py
+++ blog_fl/blog_fl/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts.urls')),  # Incluir URLs de la aplicación posts
 ]
+
+
